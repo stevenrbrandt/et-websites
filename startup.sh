bf78f62fac94949a74d517dc0c612362a0d4de22
Steven R. Brandt <sbrandt@cct.lsu.edu>
2018-04-18 16:40:08 -0500

    Initial working version of the ETK website.

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Detect if the key is there...
+if [ -r /etc/pki-etk/tls/private/einsteintoolkit.org.key ]
+then
+    # If it is, enable ssl
+    cp etk-ssl.conf /etc/apache2/conf-enabled/
+fi
+/usr/sbin/apache2ctl -D FOREGROUND
