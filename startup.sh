acc76d67d1b6d7acfbdcac67dea0c6a2effd6c51
Steven R. Brandt <sbrandt@cct.lsu.edu>
2018-07-23 21:35:47 -0400

    Update on startup. Also, include libdate-manip-perl, needed by the testsuite results page.

--- startup.sh
+++ startup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Detect if the key is there...
+cd /var/www/einstein/live
+svn update
+cd /var/www/einstein/live/manifest
+git pull
+cd /var/www/einstein/www
+git pull
+cd /var/www/einstein/seminars
+git pull
+cd /var/www/einstein/testsuite_results
+git pull
 if [ -r /etc/pki-etk/tls/private/einsteintoolkit.org.key ]
 then
     # If it is, enable ssl
