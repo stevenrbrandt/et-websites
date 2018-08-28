43fb40883522d6f91571b202565c59b298aaa66a
Steven R. Brandt <sbrandt@cct.lsu.edu>
2018-08-28 10:38:54 -0500

    Add perl lib Auto update of repos on boot Add wiki site

--- wiki/startup.sh
+++ wiki/startup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd /root
-chsh www-data --shell /bin/bash
-su -l www-data -c /var/www/einstein/update
-chsh www-data --shell /bin/false
 # Detect if the key is there...
 if [ -r /etc/pki-etk/tls/private/einsteintoolkit.org.key ]
 then
