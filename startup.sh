a916c7fb5e1443e5e02da0f491b7019d93d79b4b
Roland Haas <rhaas@mail.ubc.ca>
2025-11-06 16:39:50 -0800

    etk-docker: work around strange error with [ -r ... ]

--- startup.sh
+++ startup.sh
@@ -4,7 +4,9 @@ chsh www-data --shell /bin/bash
 su -l www-data -c /var/www/einstein/update
 chsh www-data --shell /bin/false
 # Detect if the key is there...
-if [ -r /etc/pki-etk/tls/private/einsteintoolkit.org.key ]
+# somehow [ -r .. ] fails even though the file is readable
+#if [ -r /etc/pki-etk/tls/private/einsteintoolkit.org.key ]
+if head &>/dev/null /etc/pki-etk/tls/private/einsteintoolkit.org.key
 then
     # If it is, enable ssl
     cp etk-ssl.conf /etc/apache2/conf-enabled/
